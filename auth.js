8e29fa64b481c1252caf2dfac35c91a1e35d5568
harshit <harshit@zeddlabz.com>
2023-11-01 18:18:01 +0530

    first commit

---
+++ auth.js
@@ -0,0 +1,139 @@
+require('dotenv').config();
+
+let express = require("express");
+let cookieParser = require("cookie-parser");
+const { createConnection } = require("mysql2");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+
+const route = express.Router();
+
+// Using environment variables for the database connection
+const db = createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+
+function authenticateToken(req, res, next) {
+  const token = req.session.jwt;
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+
+route.get("/test", (req, res) => {
+  const q = "Select * FROM users";
+  console.log("log: show table");
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+route.post("/signup", (req, res) => {
+  bcrypt.hash(req.body.password, 10, function (err, hash) {
+    const q = "INSERT INTO users (`username`, `password`, `email`) VALUES (?)";
+    const values = [req.body.username, hash, req.body.email];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      res.send(data);
+    });
+  });
+});
+
+route.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const queryPromise = new Promise((resolve, reject) => {
+        db.query("SELECT * FROM users WHERE username = ?;", [username], (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                if (results[0]) {
+                    resolve(results);
+                } else {
+                    res.send(false);
+                }
+            }
+        });
+    });
+    const loginPromise = queryPromise
+        .then((results) => {
+            const result = results[0]; // Assuming you want the first row
+            
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, result.password, (err, isMatch) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({ isMatch, userId: result.userId, username: result.username });
+                    }
+                });
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error occurred");
+    });
+  loginPromise
+    .then(({ isMatch, userId, username }) => {
+      if (isMatch) {
+        const user = { id: userId, name: username };
+        const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+        // res.cookie("jwt", accessToken, { httpOnly: true, sameSite: 'strict', secure: true });
+        req.session.jwt = accessToken;
+        res.send(true);
+      } else {
+        res.send(false);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error occurred");
+    });
+});
+
+route.get("/fetchCurrentUser", authenticateToken, (req, res) => {
+  res.send(req.user);
+});
+
+route.get("/isLoggedIn", (req, res) => {
+  console.log("Login checking");
+  if (req.session.jwt) {
+    res.send(true);
+  } else {
+    res.send(false);
+  }
+});
+
+function authenticateToken(req, res, next) {
+  const token = req.session.jwt;
+  if (token == null) return false;
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+route.get("/clear", (req, res) => {
+  // res.clearCookie("jwt", { path: "/" });
+  req.session.jwt = null;
+  res.send(true);
+});
+
+
+module.exports = route;
---
+++ index.js
@@ -0,0 +1,226 @@
+require('dotenv').config();
+
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const { createConnection } = require("mysql2");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const rateLimit = require("express-rate-limit");
+const path = require("path");
+const authRoutes = require("./auth");
+const session = require("express-session");
+
+const app = express();
+
+// Using environment variables for the database connection
+const db = createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.static(path.join(__dirname, "./build")));
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "session",
+    cookie: {
+      // maxAge: 30 * 60 * 60 * 1000,
+      sameSite: "lax",
+      // secure: true,
+    },
+  })
+);
+
+// const corsOptions = {
+//   origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
+//   credentials: true
+// };
+// app.use(cors(corsOptions));
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+
+// Rate Limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
+
+function authenticateToken(req, res, next) {
+  const token = req.session.jwt;
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/test", (req, res) => {
+  const q = "Select * FROM users";
+  console.log("log: show table");
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("/api/biotechnology", authenticateToken, (req, res) => {
+  const q =
+    "SELECT units.unit_id, units.unit_name, units.unit_description, lessons.lesson_id, lessons.lesson_name FROM bio_units AS units JOIN bio_lessons AS lessons ON units.unit_id = lessons.unit_id;";
+  var unitData = [];
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    unitData = data;
+    var usedUnits = [];
+    var units = [];
+    for (var lesson = 0; lesson < unitData.length; lesson++) {
+      var curr_lesson = unitData[lesson];
+      if (usedUnits.includes(curr_lesson.unit_id)) {
+        units[parseInt(curr_lesson.unit_id) - 1].lessons.push({
+          lesson_name: curr_lesson.lesson_name,
+          lesson_id: curr_lesson.lesson_id,
+          unit_id: curr_lesson.unit_id,
+        });
+      } else {
+        usedUnits.push(curr_lesson.unit_id);
+        units.push({
+          id: curr_lesson.unit_id.toString(),
+          name: curr_lesson.unit_name,
+          lessons: [
+            {
+              lesson_name: curr_lesson.lesson_name,
+              lesson_id: curr_lesson.lesson_id,
+              unit_id: curr_lesson.unit_id,
+            },
+          ],
+          description: curr_lesson.unit_description,
+        });
+      }
+    }
+    return res.json(units);
+  });
+});
+
+app.get("/api/appliedmath", authenticateToken, (req, res) => {
+  const q =
+    "SELECT units.unit_id, units.unit_name, units.unit_description, lessons.lesson_id, lessons.lesson_name FROM math_units AS units JOIN math_lessons AS lessons ON units.unit_id = lessons.unit_id;";
+  var unitData = [];
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    unitData = data;
+    var usedUnits = [];
+    var units = [];
+    for (var lesson = 0; lesson < unitData.length; lesson++) {
+      var curr_lesson = unitData[lesson];
+      if (usedUnits.includes(curr_lesson.unit_id)) {
+        units[parseInt(curr_lesson.unit_id) - 1].lessons.push({
+          lesson_name: curr_lesson.lesson_name,
+          lesson_id: curr_lesson.lesson_id,
+          unit_id: curr_lesson.unit_id,
+        });
+      } else {
+        usedUnits.push(curr_lesson.unit_id);
+        units.push({
+          id: curr_lesson.unit_id.toString(),
+          name: curr_lesson.unit_name,
+          lessons: [
+            {
+              lesson_name: curr_lesson.lesson_name,
+              lesson_id: curr_lesson.lesson_id,
+              unit_id: curr_lesson.unit_id,
+            },
+          ],
+          description: curr_lesson.unit_description,
+        });
+      }
+    }
+    return res.json(units);
+  });
+});
+
+app.get("/api/biocards", authenticateToken, (req, res) => {
+  const q = "SELECT * FROM bio_cards";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    const cardData = data;
+    const lessons = [];
+    var usedLessons = [];
+    for (var card = 0; card < cardData.length; card++) {
+      var curr_card = cardData[card];
+      if (usedLessons.includes(curr_card.lesson_id)) {
+        lessons[curr_card.lesson_id - 1].push(curr_card);
+      } else {
+        usedLessons.push(curr_card.lesson_id);
+        lessons.push([curr_card]);
+      }
+    }
+    return res.json(lessons);
+  });
+});
+
+app.get("/api/mathcards", authenticateToken, (req, res) => {
+  const q = "SELECT * FROM math_cards";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    const cardData = data;
+    const lessons = [];
+    var usedLessons = [];
+    for (var card = 0; card < cardData.length; card++) {
+      var curr_card = cardData[card];
+      if (usedLessons.includes(curr_card.lesson_id)) {
+        lessons[curr_card.lesson_id - 1].push(curr_card);
+      } else {
+        usedLessons.push(curr_card.lesson_id);
+        lessons.push([curr_card]);
+      }
+    }
+    return res.json(lessons);
+  });
+});
+
+app.get("/api/biolessons", authenticateToken, (req, res) => {
+  const q = "Select * FROM bio_lessons";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("/api/mathlessons", authenticateToken, (req, res) => {
+  const q = "Select * FROM math_lessons";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.listen(8800, () => {
+  console.log("Server running on port 8800");
+});
